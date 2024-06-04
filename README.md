e86bb3998c68d7e7dd9c9f722960fbf0e7997b93
brinkqiang <brink.qiang@gmail.com>
2024-06-04 15:44:59 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # dmicmp
+
+Copyright (c) 2013-2018 brinkqiang (brink.qiang@gmail.com)
+
+[![dmicmp](https://img.shields.io/badge/brinkqiang-dmicmp-blue.svg?style=flat-square)](https://github.com/brinkqiang/dmicmp)
+[![License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://github.com/brinkqiang/dmicmp/blob/master/LICENSE)
+[![blog](https://img.shields.io/badge/Author-Blog-7AD6FD.svg)](https://brinkqiang.github.io/)
+[![Open Source Love](https://badges.frapsoft.com/os/v3/open-source.png)](https://github.com/brinkqiang)
+[![GitHub stars](https://img.shields.io/github/stars/brinkqiang/dmicmp.svg?label=Stars)](https://github.com/brinkqiang/dmicmp) 
+[![GitHub forks](https://img.shields.io/github/forks/brinkqiang/dmicmp.svg?label=Fork)](https://github.com/brinkqiang/dmicmp)
+
+## Build status
+| [Linux][lin-link] | [Mac][mac-link] | [Windows][win-link] |
+| :---------------: | :----------------: | :-----------------: |
+| ![lin-badge]      | ![mac-badge]       | ![win-badge]        |
+
+[lin-badge]: https://github.com/brinkqiang/dmicmp/workflows/linux/badge.svg "linux build status"
+[lin-link]:  https://github.com/brinkqiang/dmicmp/actions/workflows/linux.yml "linux build status"
+[mac-badge]: https://github.com/brinkqiang/dmicmp/workflows/mac/badge.svg "mac build status"
+[mac-link]:  https://github.com/brinkqiang/dmicmp/actions/workflows/mac.yml "mac build status"
+[win-badge]: https://github.com/brinkqiang/dmicmp/workflows/win/badge.svg "win build status"
+[win-link]:  https://github.com/brinkqiang/dmicmp/actions/workflows/win.yml "win build status"
+
+## Intro
+dmicmp
+```cpp
+```
+## Contacts
+[![Join the chat](https://badges.gitter.im/brinkqiang/dmicmp/Lobby.svg)](https://gitter.im/brinkqiang/dmicmp)
+
+## Thanks
---
+++ example/ping/ping.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "icmplib.h"
+
+int main(int argc, char *argv[])
+{
+    std::string address = "8.8.8.8", resolved;
+    if (argc > 1) { address = argv[1]; }
+    try {
+        if (!icmplib::IPAddress::IsCorrect(address, icmplib::IPAddress::Type::Unknown)) {
+            resolved = address; address = icmplib::IPAddress(address);
+        }
+    } catch (...) {
+        std::cout << "Ping request could not find host " << address << ". Please check the name and try again." << std::endl;
+        return 1;
+    }
+
+    int ret = EXIT_SUCCESS;
+    std::cout << "Pinging " << (resolved.empty() ? address : resolved + " [" + address + "]")
+              << " with " << ICMPLIB_PING_DATA_SIZE << " bytes of data:" << std::endl;
+    auto result = icmplib::Ping(address, ICMPLIB_TIMEOUT_1S);
+    switch (result.response) {
+    case icmplib::PingResponseType::Failure:
+        std::cout << "Network error." << std::endl;
+        ret = EXIT_FAILURE;
+        break;
+    case icmplib::PingResponseType::Timeout:
+        std::cout << "Request timed out." << std::endl;
+        break;
+    default:
+        std::cout << "Reply from " << static_cast<std::string>(result.address) << ": ";
+        switch (result.response) {
+        case icmplib::PingResponseType::Success:
+            std::cout << "time=" << result.delay;
+            if (result.address.GetType() != icmplib::IPAddress::Type::IPv6) {
+                std::cout << " TTL=" << static_cast<unsigned>(result.ttl);
+            }
+            break;
+        case icmplib::PingResponseType::Unreachable:
+            std::cout << "Destination unreachable.";
+            break;
+        case icmplib::PingResponseType::TimeExceeded:
+            std::cout << "Time exceeded.";
+            break;
+        default:
+            std::cout << "Response not supported.";
+        }
+        std::cout << std::endl;
+    }
+    return ret;
+}
