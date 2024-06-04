5807a03d0670f18f6d0a29fed348de83d92b7a11
brinkqiang <brink.qiang@gmail.com>
2024-06-04 16:34:45 +0800

    add

--- example/ping/ping.cpp
+++ example/ping/ping.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include "dmicmp.hpp"
 #include "CLI/CLI11.hpp"
-int ping(const std::string& url)
+int ping(const std::string& url, int count)
 {
     std::string resolved;
     std::string address = url;
@@ -17,35 +17,39 @@ int ping(const std::string& url)
     int ret = EXIT_SUCCESS;
     std::cout << "Pinging " << (resolved.empty() ? address : resolved + " [" + address + "]")
               << " with " << ICMPLIB_PING_DATA_SIZE << " bytes of data:" << std::endl;
-    auto result = icmplib::Ping(address, ICMPLIB_TIMEOUT_1S);
-    switch (result.response) {
-    case icmplib::PingResponseType::Failure:
-        std::cout << "Network error." << std::endl;
-        ret = EXIT_FAILURE;
-        break;
-    case icmplib::PingResponseType::Timeout:
-        std::cout << "Request timed out." << std::endl;
-        break;
-    default:
-        std::cout << "Reply from " << static_cast<std::string>(result.address) << ": ";
-        switch (result.response) {
-        case icmplib::PingResponseType::Success:
-            std::cout << "time=" << result.delay;
-            if (result.address.GetType() != icmplib::IPAddress::Type::IPv6) {
-                std::cout << " TTL=" << static_cast<unsigned>(result.ttl);
-            }
-            break;
-        case icmplib::PingResponseType::Unreachable:
-            std::cout << "Destination unreachable.";
-            break;
-        case icmplib::PingResponseType::TimeExceeded:
-            std::cout << "Time exceeded.";
-            break;
-        default:
-            std::cout << "Response not supported.";
-        }
-        std::cout << std::endl;
-    }
+
+	for (int i=0; i < count; ++i)
+	{
+		auto result = icmplib::Ping(address, ICMPLIB_TIMEOUT_1S);
+		switch (result.response) {
+		case icmplib::PingResponseType::Failure:
+			std::cout << "Network error." << std::endl;
+			ret = EXIT_FAILURE;
+			break;
+		case icmplib::PingResponseType::Timeout:
+			std::cout << "Request timed out." << std::endl;
+			break;
+		default:
+			std::cout << "Reply from " << static_cast<std::string>(result.address) << ": ";
+			switch (result.response) {
+			case icmplib::PingResponseType::Success:
+				std::cout << "time=" << result.delay;
+				if (result.address.GetType() != icmplib::IPAddress::Type::IPv6) {
+					std::cout << " TTL=" << static_cast<unsigned>(result.ttl);
+				}
+				break;
+			case icmplib::PingResponseType::Unreachable:
+				std::cout << "Destination unreachable.";
+				break;
+			case icmplib::PingResponseType::TimeExceeded:
+				std::cout << "Time exceeded.";
+				break;
+			default:
+				std::cout << "Response not supported.";
+			}
+			std::cout << std::endl;
+		}
+	}
 
     return ret;
 }
@@ -54,7 +58,11 @@ int main(int argc, char *argv[])
     CLI::App app("ping");
 
     std::string address = "8.8.8.8";
-    app.add_option("-h", address, "Hostname or IP address to ping");
+	int count = 4;
+    app.add_option("address", address, "Hostname or IP address to ping");
+	app.add_option("-t", count, "Ping 指定运行次数，直到停止");
 
-    return ping(address);
+	CLI11_PARSE(app, argc, argv);
+	std::cout << count << std::endl;
+    return ping(address, count);
 }
